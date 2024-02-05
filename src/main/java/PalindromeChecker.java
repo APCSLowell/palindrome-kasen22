b295f77597149b5d1aa69f370ff57d9ad43a1cc2
kasen22 <143123847+kasen22@users.noreply.github.com>
2024-02-05 11:18:07 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -35,15 +35,55 @@ public void tester()
     }
   }
 }
+public void setup()
+{
+  String lines[] = loadStrings("palindromes.txt");
+  System.out.println("there are " + lines.length + " lines");
+  for (int i=0; i < lines.length; i++)
+  {
+    if(palindrome(lines[i])==true)
+    {
+      System.out.println(lines[i] + " IS a palindrome.");
+    }
+    else
+    {
+      System.out.println(lines[i] + " is NOT a palindrome.");
+    }
+  }
+}
 public boolean palindrome(String word)
 {
   //your code here
+  String backwards = onlyLetters(word.toLowerCase());
+
+
+  if (backwards.equals(reverse(backwards))) {
+   
+    return true;
+    }
+   
+ 
   return false;
 }
+
 public String reverse(String str)
 {
     String sNew = new String();
-    //your code here
+ 
+  for (int i = str.length()-1; i >= 0; i-- ) {
+    sNew += str.substring(i,i+1);
+  }
+ 
     return sNew;
+}
+
+public String onlyLetters(String sString){
+  String s = "";
+  for ( int i = 0; i < sString.length(); i++) {
+    if (Character.isLetter(sString.charAt(i))) {
+       s += sString.charAt(i);
+    }
+  }
+  return s;
 }
 }
